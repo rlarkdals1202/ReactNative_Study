bfb9846042a2aab9f91363bff71c09af2a4adc13
Kim Kangmin <rlarkdals1202@gmail.com>
2024-06-13 23:19:04 +0900

    Feat: 버튼 영역 추가

--- rn-calculator/App.tsx
+++ rn-calculator/App.tsx
@@ -1,28 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import NumberButton from '@/Components/NumberButton';
-import OperatorButton from '@/Components/OperatorButton';
+import ButtonArea from '@/Components/ButtonArea';
+import ResultArea from '@/Components/ResultArea';
 
 export default function App() {
-    const handleNumberButtonClick = (value: number) => {
-        console.log(value);
-    };
-
-    const handleOperatorButtonClick = (value: string) => {
-        console.log(value);
-    };
-
     return (
         <View style={styles.layout}>
             <StatusBar style={'auto'} />
-            <Text>Calculator Application</Text>
-            <NumberButton numberValue={0} onPress={handleNumberButtonClick} />
-            <NumberButton numberValue={1} onPress={handleNumberButtonClick} />
-            <NumberButton numberValue={2} onPress={handleNumberButtonClick} />
-            <OperatorButton operatorValue={'+'} onPress={handleOperatorButtonClick} />
-            <OperatorButton operatorValue={'-'} onPress={handleOperatorButtonClick} />
-            <OperatorButton operatorValue={'×'} onPress={handleOperatorButtonClick} />
-            <OperatorButton operatorValue={'÷'} onPress={handleOperatorButtonClick} />
+            <ButtonArea />
         </View>
     );
 }
